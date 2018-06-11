6eccb26f1bd05f78acfc274fe6dc2e7562227814
ronghuaxueleng <caoqianghappy@126.com>
2018-06-11 13:17:25 +0800

    添加了redisdemo

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # java代码样例
 > 这个项目是我平时学习java时写的一些demo样例，整体是一个多模块的maven工程，现在已经添加的模块如下：
  netty
+ redis
--- redis/src/main/java/com/yan/examples/java/redis/demo1/RedisPoolTest.java
+++ redis/src/main/java/com/yan/examples/java/redis/demo1/RedisPoolTest.java
@@ -2,7 +2,7 @@ package com.yan.examples.java.redis.demo1;
 
 public class RedisPoolTest {
 	public static void main(String[] args) {
-        RedisPool.getJedis().set("name","陈浩翔");
+        RedisPool.getJedis().set("name","caoqiang");
         System.out.println(RedisPool.getJedis().get("name"));
     }
 }
